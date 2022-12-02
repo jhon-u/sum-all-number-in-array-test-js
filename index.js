b1fe1b7a27ae71d34497b93e1130373cf867586c
Jhon Useche <jhon.useche@telus.com>
2022-12-02 08:51:19 -0700

    Commited

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let result = 0;
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      result += sumItems(item);
+    } else {
+      result += item;
+    }
+  }
+
+  return result;
+};
 
 module.exports = sumItems;
